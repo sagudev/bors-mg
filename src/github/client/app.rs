e4deeec529820bfeec7d39310124da5da5880aad
sagudev <16504129+sagudev@users.noreply.github.com>
2023-11-29 10:27:45 +0100

    bk

--- src/github/client/app.rs
+++ src/github/client/app.rs
@@ -108,3 +108,68 @@ impl GitHubClient for AppClient {
             .map_err(|e| anyhow::anyhow!(e))
     }
 }
+
+#[tokio::test]
+async fn pong() {
+    APP_ID.set("355259".to_string()).unwrap();
+    PRIVATE_KEY
+        .set(
+            "-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEAwjn8AcClx5lL/AfEGMUjpKwifIF5jkbepmFiMHTNjtHIegy6
+O7xCgo2EjgnNDxs1P8ovJ46jM/kOVQbbrmiaU1iFgRPwBlTEkTxv3LxVnIYYBeAo
+3BZT7mltbCUXyFB93RO8NQBbKXXJDrk4pG1rwO6eUwbLk/mT2Y0SJ7ldnHjBxmPT
+JceYZhtHvZCMPzdFu/D8iBjZwQBYV6/r1NFNtS7C0wTaKeikPvghXpiWuRVBC7vj
+FEMLUWet9XrS2O2/2sMhh3ctb80XwVbfV6EvSp5WLAvK3gGPq9RAu2KGco80NZY+
+Nbd6PDrauOYw6SZVTi7sLKfis8GJpr4wUPD61wIDAQABAoIBABgHbHIbD2d+Q7RO
+kagu25YH5cxZiFxd0DXtXmR4TuYYdiEDahbx15inQXsBI2l3CEN4FBVkxDQt3+QN
+ESimsFEXZoztlohx/E+rlntMoZrXzCkN7oAsEv4v9OWoQST7MFclledIv/6FH/a+
+W4+cKfSYkOXctVr9SZlkppZbjIVLBBmtGHxOkBgvY5Bb63wKL5RtXrt7TFO1LI2Q
+8rfEdr/2zQ3PQ3b1L7TocneYTRccHeV38nJfztvT4HPEaStR6ZsM75majHyAn28L
+W2gQUI+eNgwpV0UUTZ8ZWXyKS+bBNEUirn45XE85oqiZ27bbu4tJtOJk4GFG4Kfd
+BFTFqYECgYEA5ozrInqgWVffYNfomSzPat7/hCFANCaBUU+Xlza2KnYoL9cvXa2q
+pqjD+KN5/lflDl0PzrUpk2WyQQBFGgf9z60flgmQ1oBFNCLU8DCWSmWkZWiqOvHd
+UmovfwbQQXyVupRgRIw2ihT0TcuVuvqdva9rP9E/MUXLjm1RD86dhocCgYEA16qY
+Eb7CUuxnOqN+Z0O7+ruymhh6sH8K9EDhYhvLnl7NlTzdp7wAJe9c+N0huyEeW83v
+ZcIrfVzosUtxLWUcPQ/Awvs+LMh4e7Bac+goDG1JlH4w1IztuIoLCYHvFQ/UgGAz
+trJ5qMc63zNkR269j3Eh4kgoEZktXSSI599XrTECgYEAvNw3ShlV/ZpEPJrhyYix
+qQRgICb050Obr7YZoh+JfqMoLHiELqMzJi8dyjJwnu/1jZyidFxnYH1wVlsYQEjH
+nDZfp5LSeUS+bAUUlmXW178HuqLQwFSdTwP5QH+eXebm6N7fNYf+rYKY1pmtYGwo
+h9iJbM/GimB4bYYTX3WMCUUCgYEArrkH4ICCYLoj287vLmwi1DzSsqMYavtR/Za+
+wkQwj8rQlZKtJSJboGAvG3PTyw5G0SujQvavUy49Wr37IELlQNcNXSo9MfzsF5FF
+htfT8lVsIkCmAN14DmTQElDRSGf9yk+mNeKcS8+083VoTbL7IkYOpIu+4psNtINP
+40L/flECgYA2LukS9pu+lmNi7e3skFojuhJOIjcDaUmeAf/HbGKjckgQO5QB2S30
+tLPcgANBANF9poWbzFxYVcchtW2wpC5eyLrztkxA3X+XKaN8DkHafdXJQHtdd2Pv
+xCUhwhCTXk7zQJqY1UKGjVEOs/1F6GITwQm1ZCC7WSdLedfoZHpe9w==
+-----END RSA PRIVATE KEY-----"
+                .to_string(),
+        )
+        .unwrap();
+    CMD_PREFIX.set("@bo-homu".to_string()).unwrap();
+    let mut cli = AppClient::new().unwrap();
+    println!("tok: {}", cli.0);
+    cli.post_comment(
+        &crate::github::GithubRepo {
+            owner: "bo-playground".to_string(),
+            name: "musical-enigma".to_string(),
+        },
+        6,
+        "Test pong",
+    )
+    .await
+    .unwrap();
+    /*let req = reqwest::Client::new()
+        .post("https://api.github.com/repos/bo-playground/musical-enigma/issues/6/comments")
+        .bearer_auth(PAT.get().unwrap())
+        .header("X-GitHub-Api-Version", "2022-11-28")
+        .header("Accept", "application/vnd.github+json")
+        .header("User-Agent", CMD_PREFIX.get().unwrap())
+        .json(&serde_json::json!({
+            "body": "Test pong 🏓",
+        }));
+    tracing::debug!("Reqq: {:#?}", req);
+    let resp = req.send().await.map_err(|e| anyhow::anyhow!(e)).unwrap();
+    println!("{resp:#?}");
+    let stst = resp.status();
+    println!("Body: {:#?}", resp.text().await);
+    assert!(stst.is_success());*/
+}
--- src/github/client/token.rs
+++ src/github/client/token.rs
@@ -58,3 +58,36 @@ impl GitHubClient for TokenClient {
             .map_err(|e| anyhow::anyhow!(e))
     }
 }
+
+#[tokio::test]
+async fn pong() {
+    PAT.set("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".to_string())
+        .unwrap();
+    CMD_PREFIX.set("@bo-homu".to_string()).unwrap();
+    let mut cli = TokenClient;
+    cli.post_comment(
+        &GithubRepo {
+            owner: "bo-playground".to_string(),
+            name: "musical-enigma".to_string(),
+        },
+        6,
+        "Test pong",
+    )
+    .await
+    .unwrap();
+    /*let req = reqwest::Client::new()
+        .post("https://api.github.com/repos/bo-playground/musical-enigma/issues/6/comments")
+        .bearer_auth(PAT.get().unwrap())
+        .header("X-GitHub-Api-Version", "2022-11-28")
+        .header("Accept", "application/vnd.github+json")
+        .header("User-Agent", CMD_PREFIX.get().unwrap())
+        .json(&serde_json::json!({
+            "body": "Test pong 🏓",
+        }));
+    tracing::debug!("Reqq: {:#?}", req);
+    let resp = req.send().await.map_err(|e| anyhow::anyhow!(e)).unwrap();
+    println!("{resp:#?}");
+    let stst = resp.status();
+    println!("Body: {:#?}", resp.text().await);
+    assert!(stst.is_success());*/
+}
